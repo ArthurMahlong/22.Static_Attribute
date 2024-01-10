60ee0ef4fa5c2fdd5c2e0442198045b25e4e52ab
Arthur <mahlo@DESKTOP-P0ONE70>
2024-01-10 14:35:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Song Holiday = new Song("Holiday", "Green Day", 5);
+            Song kashmir = new Song("Kashmir", "Zeppelin", 4);
+            Song Mnike = new Song("Mniker", "Tyler", 3);
+
+            Console.WriteLine(Song.songCount);
+            Console.Read();
+        }
+    }
+}
